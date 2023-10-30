9fcb9e4cfbdbaaed1220ca0ebb548fd453729138
Vonique Stricklen <vstrickl.git@gmail.com>
2023-10-29 23:42:26 -0700

    Adjusted responsive style of calendar

--- schedule/views.py
+++ schedule/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from meet.models import Header
+from meet.models import Header, Menu
 
 # Create your views here.
 def schedule(request):
 
     header = Header.objects.all()
+    menu = Menu.objects.all()
+
     google_api_key = settings.GOOGLE_API_KEY
     google_calendar_id = settings.GOOGLE_CALENDAR_ID
 
     context = {
         'header':header,
+        'menu':menu,
         'google_api_key':google_api_key,
         'google_calendar_id':google_calendar_id,
     }
