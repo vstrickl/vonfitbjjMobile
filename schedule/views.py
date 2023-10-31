cc0c4cc204a2e5729839c16775b5b4b00d224b5e
Vonique Stricklen <vstrickl.git@gmail.com>
2023-10-31 00:39:00 -0700

    Updated app for responsive design.
    - Added responsive collapsible menu

--- schedule/views.py
+++ schedule/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from meet.models import Header, Menu
+from meet.models import Header, Menu, SocialMenu
 
 # Create your views here.
 def schedule(request):
 
-    header = Header.objects.all()
+    title = 'Schedule'
+
     menu = Menu.objects.all()
+    header = Header.objects.all()
+    social = SocialMenu.objects.all()
 
     google_api_key = settings.GOOGLE_API_KEY
     google_calendar_id = settings.GOOGLE_CALENDAR_ID
 
     context = {
-        'header':header,
         'menu':menu,
+        'title': title,
+        'header':header,
+        'social':social,
         'google_api_key':google_api_key,
         'google_calendar_id':google_calendar_id,
     }
